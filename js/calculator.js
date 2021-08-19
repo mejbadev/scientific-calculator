f6cc627f8823a949424f776edbcbfd620e8e7f42
mejbadev <mejbacsedu@gmail.com>
2021-08-19 12:52:24 +0600

    Uploaded

---
+++ js/calculator.js
@@ -0,0 +1,18 @@
+const inputField = document.getElementById('input-string');
+        document.getElementById('input-part').addEventListener('click', function(event){
+                if(typeof event.target.value !='undefined' && event.target.value !='clear' && event.target.value !='='){
+                    inputField.value+=event.target.value;
+                }else if(event.target.value=='clear'){
+                    inputField.value='';
+                }else if(event.target.value =='='){
+                    getResult(inputField.value);
+                }
+        })
+        function getResult(inputText){
+            let inputEquation= inputText.replace(/cos|sin|tan|sqrt/gi, function(x){
+                return x.replace(x, 'Math.'+x);
+            })
+            const result= eval(inputEquation);
+            const displayResult= document.getElementById('input-string');
+            displayResult.value=result+'';  
+        }
